d31c24f270828c8ebb1b1f9a368345e33840cc07
Bailey Townsend <baileytownsend2323@gmail.com>
2023-02-16 15:28:54 -0600

    finished up front end minus web calls

--- vue-project/src/components/todoItemComponent.vue
+++ vue-project/src/components/todoItemComponent.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import type {Todo} from '@/dtos/todo';
+  import {useTodoStore} from '@/stores/todo';
+  const todoStore = useTodoStore();
 
   const props = defineProps<{
     todo: Todo,
@@ -12,18 +14,16 @@
     <p :class="['w-full', {'line-through': props.todo.completed}]">{{props.todo.description}}</p>
 
 
-    <button v-if="props.todo.completed"
-            class="ml-4 mr-2 btn btn-outline primary"
-            name="not_done">Undo</button>
-    <button class="ml-4 mr-2 btn btn-outline btn-success"
+    <button v-if="!props.todo.completed"
+            class="ml-4 mr-2 btn btn-outline btn-success"
+            @click="todoStore.completeTodo(props.todo.id)"
             name="done">Done</button>
 
     <button class="ml-2 btn btn-outline btn-error"
+            @click="todoStore.removeTodo(props.todo)"
             name="remove">Remove</button>
 
-
   </div>
-
 </template>
 
 
--- vue-project/src/stores/todo.ts
+++ vue-project/src/stores/todo.ts
@@ -9,9 +9,25 @@ export const useTodoStore = defineStore('todo', () => {
   const todos = ref<Array<Todo>>([]);
 
   function addTodo(todo: Todo) {
-
+    //TODO make fake id right now. Will get id from api in long run
+    todo.id = Math.floor(Math.random() * 10000000);
     todos.value.push(todo);
   }
 
-  return { todos, addTodo };
+  function removeTodo(todo: Todo) {
+    const index = todos.value.indexOf(todo);
+    if (index !== -1) {
+      todos.value.splice(index, 1);
+    }
+
+  }
+
+  function completeTodo(id: number) {
+    const todo = todos.value.find(todo => todo.id === id);
+    if(todo){
+      todo.completed = true;
+    }
+  }
+
+  return { todos, addTodo, removeTodo, completeTodo };
 });
