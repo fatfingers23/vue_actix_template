9403d7485d1e8fce8ef1a2ebc9b6dd6f3c383a69
Bailey Townsend <baileytownsend2323@gmail.com>
2023-02-16 21:01:01 -0600

    We have async

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,5 @@ extern crate core;
 
 pub mod api;
 pub mod container;
-pub mod create_app;
 mod data_access;
 pub mod domain;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,41 @@
-#![feature(async_closure)]
-
-use actix_web::HttpServer;
+use actix_files::{Files, NamedFile};
+use actix_web::middleware::Logger;
+use actix_web::{guard, Error, HttpServer};
+use actix_web::{web, App};
 use dotenv::dotenv;
+use vue_actix_template::api::controllers::hello_world_controller::{
+    repeat_handler, say_hello_handler,
+};
+use vue_actix_template::api::controllers::todo_controller::list_todos_handler;
 use vue_actix_template::container::Container;
-use vue_actix_template::create_app::create_app;
 
 // #[cfg(test)]
 // mod tests;
+
+async fn index() -> Result<NamedFile, Error> {
+    Ok(NamedFile::open("./spa/index.html")?)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init();
-    let server = HttpServer::new(move || create_app().bind(("127.0.0.1", 8080)));
+
+    let container = Container::new().await;
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::from(container.todo_repository.clone()))
+            .wrap(Logger::default())
+            .service(
+                web::scope("/api")
+                    .service(say_hello_handler)
+                    .service(repeat_handler)
+                    .service(web::scope("/todo").service(list_todos_handler)),
+            )
+            .service(Files::new("/", "./spa").index_file("index.html"))
+            .default_service(web::route().guard(guard::Not(guard::Get())).to(index))
+    })
+    .bind(("127.0.0.1", 8080));
     server.unwrap().run().await
 }
